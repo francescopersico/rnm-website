2f4f55d2025f176bee69a0fc6abd9aeef8ba7666
Francesco Persico <francescopersico@gmail.com>
2023-08-10 12:26:55 +0200

    Setup Nextra (#3)

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>React Native Microfrontends</span>,
+  project: {
+    link: "https://github.com/reactnativemicrofrontends/website",
+  },
+  docsRepositoryBase: "https://github.com/reactnativemicrofrontends/website",
+};
+
+export default config;
