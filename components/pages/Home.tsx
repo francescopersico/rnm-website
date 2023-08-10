fb152b9661a88bac68344807d3185d00e8be1c7d
Francesco Persico <francescopersico@gmail.com>
2023-08-10 12:24:24 +0200

    Setup Nextra

---
+++ components/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="container mx-auto">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl">React Native Microfrontends</h1>
+
+        <p className="p-3">Welcome to React Native Microfrontends!</p>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withNextra({
+  reactStrictMode: true,
+});
+
+module.exports = nextConfig;
