c760932d604bef8a553a7555c70cddd51f4d1388
Francesco Persico <francescopersico@gmail.com>
2023-08-11 17:15:20 +0200

    Update home hero (#5)

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -5,27 +5,21 @@ import MobileMicrofrontendsImg from "../../public/images/mobile-microfrontends.p
 
 const Hero = () => {
   return (
-    <section className="container mx-auto p-8 flex flex-wrap">
-      <div className="flex w-full lg:w-1/2 items-center">
-        <div className="mb-5">
-          <h1 className="text-4xl font-extrabold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-            React Native Microfrontends
-          </h1>
-          <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-            A full website about how to build robust and well-architected mobile
-            microfrontends using React Native.
-          </p>
-        </div>
-      </div>
-      <div className="flex w-full lg:w-1/2 items-center justify-center">
-        <div>
-          <Image
-            src={MobileMicrofrontendsImg}
-            alt="Mobile Microfrontends"
-            className="object-cover"
-            priority={true}
-          />
-        </div>
+    <section className="container mx-auto p-8 justify-center content-center">
+      <div className="flex flex-col  items-center justify-center px-8">
+        <Image
+          src={MobileMicrofrontendsImg}
+          alt="Mobile Microfrontends"
+          className="object-cover"
+          priority={true}
+        />
+        <h1 className="text-3xl font-extrabold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+          React Native Microfrontends
+        </h1>
+        <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+          A full website about how to build complex and well-architected mobile
+          microfrontends using React Native.
+        </p>
       </div>
     </section>
   );
