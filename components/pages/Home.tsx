121c2974998b823ace103699103e43b9f2871b85
Francesco Persico <francescopersico@gmail.com>
2023-08-12 00:25:58 +0200

    Improve hero (#7)

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -6,7 +6,7 @@ import MobileMicrofrontendsImg from "../../public/images/mobile-microfrontends.p
 const Hero = () => {
   return (
     <section className="container mx-auto p-8 justify-center content-center">
-      <div className="flex flex-col  items-center justify-center px-8">
+      <div className="flex flex-col items-center justify-center px-8">
         <Image
           src={MobileMicrofrontendsImg}
           alt="Mobile Microfrontends"
@@ -16,9 +16,11 @@ const Hero = () => {
         <h1 className="text-3xl font-extrabold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
           React Native Microfrontends
         </h1>
-        <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+        <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300 lg:max-w-5xl">
           A full website about how to build complex and well-architected mobile
-          microfrontends using React Native.
+          microfrontends using React Native. From basic concepts to advanced
+          topics like microfrontend complex architecture, state management, navigation, CI/CD,
+          team organization/scaling and much much more.
         </p>
       </div>
     </section>
